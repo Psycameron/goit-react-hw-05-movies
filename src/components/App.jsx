6ed6e0388b64e45c3cc9ba2eecd6d47c2bd9a4ad
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-03-08 19:26:11 +0200

    Refactored routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,9 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import Cast from './Cast/Cast';
-import MovieDetails from './MovieDetails/MovieDetails';
 import Reviews from './Reviews/Reviews';
+import NotFound from './NotFound/NotFound';
+import MovieDetails from '../pages/MovieDetails';
 
 export const App = () => {
   return (
@@ -17,12 +18,12 @@ export const App = () => {
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />}>
-          <Route path=":movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
+        <Route path="/movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
